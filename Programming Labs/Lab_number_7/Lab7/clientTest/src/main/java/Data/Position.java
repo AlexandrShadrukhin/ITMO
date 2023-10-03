239f39dc69a9ab9d4c03fc8ce79e759f0738b6f0
AlexandrShadrukhin <114620271+AlexandrShadrukhin@users.noreply.github.com>
2023-10-03 23:04:16 +0300

    Delete Programming Labs/Lab_number_7/Lab7/clientTest directory

--- Programming Labs/Lab_number_7/Lab7/clientTest/src/main/java/Data/Position.java
+++
@@ -1,9 +0,0 @@
-package Data;
-
-import java.io.Serializable;
-
-public enum Position implements Serializable {
-    DIRECTOR,
-    HEAD_OF_DIVISION,
-    LEAD_DEVELOPER;
-}
--- Programming Labs/Lab_number_7/Lab7/clientTest/src/main/java/Exceptions/ValueException.java
+++
@@ -1,4 +0,0 @@
-package Exceptions;
-
-public class ValueException extends RuntimeException{
-}
