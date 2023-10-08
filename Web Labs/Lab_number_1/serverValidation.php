3c9b5f973793ba523bb56c25ee9a23fb2dde75c4
AlexandrShadrukhin <114620271+AlexandrShadrukhin@users.noreply.github.com>
2023-10-08 23:37:28 +0300

    Update serverValidation.php

--- Web Labs/Lab_number_1/serverValidation.php
+++ Web Labs/Lab_number_1/serverValidation.php
@@ -1,4 +1,29 @@
 <?php
+function validateInput($x, $y, $r, $time) {
+    $errors = array();
+
+    if (!filter_var($x, FILTER_VALIDATE_FLOAT) || $x < -5 || $x > 3) {
+        $errors[] = "Неверное значение X";
+    }
+
+    if (!filter_var($y, FILTER_VALIDATE_FLOAT) || $y < -3 || $y > 5) {
+        $errors[] = "Неверное значение Y";
+    }
+
+    if (!filter_var($r, FILTER_VALIDATE_FLOAT) || $r < 2 || $r > 5) {
+        $errors[] = "Неверное значение R";
+    }
+
+    if (!filter_var($time, FILTER_VALIDATE_FLOAT) || $time < 0 || $time > 24) {
+        $errors[] = "Неверное значение Time";
+    }
+
+    if (!empty($errors)) {
+        http_response_code(400);
+        exit(implode(", ", $errors));
+    }
+}
+
 function validate($x, $y, $r) {
     if ($x==0 && $y<=$r/2 && $y>=-$r){
         return true;
