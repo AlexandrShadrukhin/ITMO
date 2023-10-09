81ef0b92f6398f375980284cb101cfa9034ac657
AlexandrShadrukhin <114620271+AlexandrShadrukhin@users.noreply.github.com>
2023-10-09 10:27:35 +0300

    Update serverValidation.php

--- Web Labs/Lab_number_1/serverValidation.php
+++ Web Labs/Lab_number_1/serverValidation.php
@@ -1,30 +1,35 @@
 <?php
-function validateInput($x, $y, $r, $time) {
-    $errors = array();
-
-    if (!filter_var($x, FILTER_VALIDATE_INT) || $x < -5 || $x > 3) {
-        $errors[] = "Неверное значение X";
-    }
-
-    if (!filter_var($y, FILTER_VALIDATE_FLOAT) || $y < -3 || $y > 5) {
-        $errors[] = "Неверное значение Y";
-    }
-
-    if (!filter_var($r, FILTER_VALIDATE_FLOAT) || $r < 2 || $r > 5) {
-        $errors[] = "Неверное значение R";
-    }
-
-    if (!filter_var($time, FILTER_VALIDATE_INT) || $time < 0 || $time > 24) {
-        $errors[] = "Неверное значение Time";
-    }
-
-    if (!empty($errors)) {
-        http_response_code(400);
-        exit(implode(", ", $errors));
-    }
+function check_x($x): bool
+{
+    return $x >= -5 and $x <= 3;
 }
 
-function validate($x, $y, $r) {
+function check_y($y): bool
+{
+    return $y >= -3 and $y <= 5;
+}
+
+function check_r($r): bool
+{
+    return $r >= 2 and $r <= 5;
+}
+
+function check_time($time): bool
+{
+    return $time >= 0 and $time <= 24;
+}
+
+function validateInput($x, $y, $r, $time): bool
+{
+    if (!(check_x($x) || check_y($y) || check_r($r) || check_time($time))) {
+        http_response_code(400);
+        die("Ошибка валидации");
+    }
+    return true;
+}
+
+function validate($x, $y, $r): bool
+{
     if ($x==0 && $y<=$r/2 && $y>=-$r){
         return true;
     } elseif ($y==0 && $x<=$r && $x>=-$r){
