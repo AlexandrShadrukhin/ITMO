6e802b6719f95fe02ebae6d4a2f820785d06c9b6
AlexandrShadrukhin <114620271+AlexandrShadrukhin@users.noreply.github.com>
2023-12-08 22:03:59 +0300

    Update AreaCheckBean.java

--- Web Labs/Lab_number_3/src/main/java/lab3/AreaCheckBean.java
+++ Web Labs/Lab_number_3/src/main/java/lab3/AreaCheckBean.java
@@ -5,18 +5,24 @@ import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.validation.ValidationException;
+import org.hibernate.HibernateError;
 import org.primefaces.PrimeFaces;
 import lab3.database.DAOFactory;
 
 import java.io.Serializable;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 @Named
 @ApplicationScoped
 public class AreaCheckBean implements Serializable {
     private LinkedList<ResultBean> resultList;
+
     @PersistenceContext
     private EntityManager entityManager;
 
@@ -27,31 +33,42 @@ public class AreaCheckBean implements Serializable {
         } catch (SQLException ignored) {}
     }
 
+
+
     public void newResult(final double x, String y, final double r) {
-
-        double numY = 0;
-
-        if (!y.isEmpty()) {
-            numY = Double.parseDouble(y);
-        }
-        final ResultBean currentResult = new ResultBean();
-        final long startExec = System.nanoTime();
-        final boolean result = checkArea(x, numY, r);
-        final long endExec = System.nanoTime();
-        final long executionTime = endExec - startExec;
-        currentResult.setX(x);
-        currentResult.setY(y);
-        currentResult.setR(r);
-        currentResult.setResult(result);
-        currentResult.setExecutedAt(LocalDateTime.now());
-        currentResult.setExecutionTime(executionTime);
-
         try {
-            DAOFactory.getInstance().getResultDAO().addNewResult(currentResult);
-        } catch (SQLException ignored) {}
+            if(validateDb((int) r, (float) x, y)) {
+                double numY = 0;
 
-        resultList.addFirst(currentResult);
-        PrimeFaces.current().executeScript("drawPoint(" + x + ", " + numY + ", '" + result + "')");
+                if (!y.isEmpty()) {
+                    numY = Double.parseDouble(y);
+                }
+                final ResultBean currentResult = new ResultBean();
+                final long startExec = System.nanoTime();
+                final boolean result = checkArea(x, numY, r);
+                final long endExec = System.nanoTime();
+                final long executionTime = endExec - startExec;
+                currentResult.setX(x);
+                currentResult.setY(y);
+                currentResult.setR(r);
+                currentResult.setResult(result);
+                currentResult.setExecutedAt(LocalDateTime.now());
+                currentResult.setExecutionTime(executionTime);
+
+                try {
+                    DAOFactory.getInstance().getResultDAO().addNewResult(currentResult);
+                } catch (SQLException ignored) {
+                }
+
+                resultList.addFirst(currentResult);
+                PrimeFaces.current().executeScript("drawPoint(" + x + ", " + numY + ", '" + result + "')");
+            }
+            else {
+                throw new ValidationException("Ошибка валидации исправьте пожалуйста");
+            }
+        }catch (HibernateError ignored){
+            
+        }
     }
 
 
@@ -71,6 +88,27 @@ public class AreaCheckBean implements Serializable {
     public boolean checkArea(double x, double y, double r) {
         return checkCircle(x, y, r) || checkTriangle(x, y, r) || checkSquare(x, y, r);
     }
+    public boolean validateDb(int r,float x, String y){
+        return (validateR(r)||validateX(x)||validateY(y));
+    }
+    
+    public boolean validateX(double x){
+        return !(x < -4) && !(x > 4);
+    }
+    public boolean validateY(String y){
+        try {
+            double Y = Double.parseDouble(y);
+            return (!(Y < -3)) && (!(Y > 3));
+        }catch (NumberFormatException e){
+            return false;
+        }
+    }
+    List<Integer> rValues = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    public boolean validateR(int r){
+        return rValues.contains(r);
+    }
+
+
 
     @Named("resultList")
     public LinkedList<ResultBean> getResultList() {
